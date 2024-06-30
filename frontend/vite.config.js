837cfeb481a556af39b70cf3836392d2d09f2023
loydie16 <johnloydtalagtag16@gmail.com>
2024-06-30 23:26:02 +0800

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,5 @@
-
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
-
-
-/* import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -31,4 +20,3 @@ export default defineConfig({
     },
   },
 });
- */
