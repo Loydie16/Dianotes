4cd3fd25572e495c128ff748e216f330ae575d1b
loydie16 <johnloydtalagtag16@gmail.com>
2024-06-30 23:22:33 +0800

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,15 @@
+
+
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
+
+
+/* import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
@@ -13,14 +24,11 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://dianotes-api.vercel.app", // Make sure this is the correct protocol (http or https)
+        target: "https://dianotes-api.vercel.app",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
-        secure: true, // Ensures that the proxy works over HTTPS if your target is using HTTPS
-        headers: {
-          "Access-Control-Allow-Origin": "*", // Optionally add headers for debugging CORS
-        },
       },
     },
   },
 });
+ */
